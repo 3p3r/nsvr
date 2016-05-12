b30ceb665da80bc3c8e97ea61c4531988278a562
Sepehr Laal <sepehr.laal@gmail.com>
2016-05-12 13:07:26 -0700

    Added a version getter and plugin helper api

--- include/nsvr.hpp
+++ include/nsvr.hpp
@@ -3,3 +3,11 @@
 #include "nsvr/nsvr_player.hpp"
 #include "nsvr/nsvr_player_client.hpp"
 #include "nsvr/nsvr_player_server.hpp"
+
+namespace nsvr
+{
+
+std::string getVersion();
+void        addPluginPath(const std::string& path);
+
+}
--- source/nsvr.cpp
+++ source/nsvr.cpp
@@ -1,3 +1,40 @@
 #include "nsvr.hpp"
+#include "nsvr/nsvr_internal.hpp"
 
-/* empty */
+namespace nsvr
+{
+
+std::string getVersion()
+{
+#   define STRINGIFO(v) #v
+#   define STRINGIFY(v) STRINGIFO v
+
+    return "1.0.0" "_"
+        STRINGIFY(GST_VERSION_MAJOR) "."
+        STRINGIFY(GST_VERSION_MINOR) "."
+        STRINGIFY(GST_VERSION_MICRO) "."
+        STRINGIFY(GST_VERSION_NANO);
+}
+
+void addPluginPath(const std::string& path)
+{
+    if (path.empty())
+    {
+        g_debug("Plug-in path supplied is empty.");
+        return;
+    }
+
+    if (!Internal::gstreamerInitialized())
+    {
+        g_debug("You are not able to add plug-in path. %s",
+            "GStreamer could not be initialized.");
+        return;
+    }
+
+    if (GstRegistry *registry = gst_registry_get())
+    {
+        gst_registry_scan_path(registry, path.c_str());
+    }
+}
+
+}
