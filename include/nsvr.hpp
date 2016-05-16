9365cdf8c2250b037bc6377c7adaf6f12d7fd44d
Sepehr Laal <sepehr.laal@gmail.com>
2016-05-16 15:23:25 -0700

    Added API to get/set default multicast IP/port

--- include/nsvr.hpp
+++ include/nsvr.hpp
@@ -4,10 +4,35 @@
 #include "nsvr/nsvr_player_client.hpp"
 #include "nsvr/nsvr_player_server.hpp"
 
+#define NSVR_VERSION_MAJOR 1
+#define NSVR_VERSION_MINOR 0
+#define NSVR_VERSION_PATCH 0
+
 namespace nsvr
 {
 
+//! Returns version of NSVR (1st.) + GStreamer (2nd.)
 std::string getVersion();
-void        addPluginPath(const std::string& path);
+
+//! Adds a plug-in path to GStreamer's search directory
+void addPluginPath(const std::string& path);
+
+//! Returns default multicast IP
+std::string getDefaultMulticastIp();
+
+//! Sets default multicast IP
+void setDefaultMulticastIp(const std::string& group);
+
+//! Returns default multicast port
+short getDefaultMulticastPort();
+
+//! Sets default multicast port
+void setDefaultMulticastPort(short port);
+
+//! Returns if default multicast is enabled (default: yes)
+bool defaultMulticastGroupEnabled();
+
+//! Sets if default multicast is enabled (default: yes)
+void enableDefaultMulticastGroup(bool on = true);
 
 }
--- source/nsvr.cpp
+++ source/nsvr.cpp
@@ -1,15 +1,43 @@
 #include "nsvr.hpp"
 #include "nsvr/nsvr_internal.hpp"
 
+#include <memory>
+
+namespace {
+
+struct DefaultMulticastGroup
+{
+    DefaultMulticastGroup(const std::string& g, short p)
+        : group(g), port(p), enabled(true)
+    { /* no-op */ }
+
+    std::string group;
+    short       port;
+    bool        enabled;
+};
+
+std::shared_ptr<DefaultMulticastGroup> getDefaultMulticastGroup()
+{
+    static std::shared_ptr<DefaultMulticastGroup> instance
+        = std::make_shared<DefaultMulticastGroup>("239.0.0.1", 5000);
+
+    return instance;
+}
+
+}
+
 namespace nsvr
 {
 
 std::string getVersion()
 {
 #   define STRINGIFO(v) #v
-#   define STRINGIFY(v) STRINGIFO v
+#   define STRINGIFY(v) STRINGIFO(v)
 
-    return "1.0.0" "_"
+    return
+        STRINGIFY(NSVR_VERSION_MAJOR) "."
+        STRINGIFY(NSVR_VERSION_MINOR) "."
+        STRINGIFY(NSVR_VERSION_PATCH) "_"
         STRINGIFY(GST_VERSION_MAJOR) "."
         STRINGIFY(GST_VERSION_MINOR) "."
         STRINGIFY(GST_VERSION_MICRO) "."
@@ -34,6 +62,42 @@ void addPluginPath(const std::string& path)
     {
         gst_registry_scan_path(registry, path.c_str());
     }
+}
+
+std::string getDefaultMulticastIp()
+{
+    static auto handle = getDefaultMulticastGroup();
+    return handle->group;
+}
+
+void setDefaultMulticastIp(const std::string& group)
+{
+    static auto handle = getDefaultMulticastGroup();
+    handle->group = group;
+}
+
+short getDefaultMulticastPort()
+{
+    static auto handle = getDefaultMulticastGroup();
+    return handle->port;
+}
+
+void setDefaultMulticastPort(short port)
+{
+    static auto handle = getDefaultMulticastGroup();
+    handle->port = port;
+}
+
+bool defaultMulticastGroupEnabled()
+{
+    static auto handle = getDefaultMulticastGroup();
+    return handle->enabled;
+}
+
+void enableDefaultMulticastGroup(bool on /*= true*/)
+{
+    static auto handle = getDefaultMulticastGroup();
+    handle->enabled = on;
 }
 
 }
